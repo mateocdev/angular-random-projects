e79f35cdaef42dfea9862c869490cfc7f90c59a2
mateocdev <m.castano1@uniandes.edu.co>
2024-04-18 09:15:24 -0500

    🚧 Skeleton for reactive forms on angular

--- formApp/src/app/reactive/reactive.module.ts
+++ formApp/src/app/reactive/reactive.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { BasicPAgeComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicPAgeComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent
+  ],
   imports: [
     CommonModule,
     ReactiveRoutingModule
