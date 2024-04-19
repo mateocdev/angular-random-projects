789edd354d0b75e2dc45e4c42845df3757826289
Mateo Castano <m.castano1@uniandes.edu.co>
2024-04-19 11:27:57 -0500

    :construction: Working on reactive form, the integration is done

--- formApp/src/app/reactive/reactive.module.ts
+++ formApp/src/app/reactive/reactive.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { ReactiveRoutingModule } from './reactive-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BasicPAgeComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
-
+import { ReactiveRoutingModule } from './reactive-routing.module';
 
 @NgModule({
   declarations: [
     BasicPAgeComponent,
     DynamicPageComponent,
-    SwitchesPageComponent
+    SwitchesPageComponent,
   ],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule
-  ]
+  imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}
