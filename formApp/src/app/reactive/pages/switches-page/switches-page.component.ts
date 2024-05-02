dfa760f137f3442ca10c6a3f53904c85620b3501
Mateo Castano <m.castano1@uniandes.edu.co>
2024-05-02 17:58:31 -0500

    :construction: Working with switcher

--- formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SwitchesPageComponent {
+  constructor(private fb: FormBuilder) {}
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    return;
+  }
 }
