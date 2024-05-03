43d37d4fb4b3635cbd7b2dee2547224db48ae4f2
Mateo Castano <m.castano1@uniandes.edu.co>
2024-05-03 11:31:20 -0500

    :construction: Working with validators

--- formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  constructor(private fb: FormBuilder) {}
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+  });
 }
--- formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   public myForm: FormGroup = this.fb.group({
@@ -13,6 +13,34 @@ export class SwitchesPageComponent {
     wantNotifications: [true, Validators.required],
     termsAndConditions: [false, Validators.requiredTrue],
   });
+
+  ngOnInit(): void {
+    this.myForm.reset({
+      gender: 'M',
+      wantNotifications: true,
+      termsAndConditions: false,
+    });
+  }
+
+  getFieldError(field: string) {
+    if (!this.myForm.controls[field].errors) {
+      return null;
+    }
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key in errors) {
+      switch (key) {
+        case 'required':
+          return 'This field is required';
+        case 'minlength':
+          return `Min length is ${errors[key].requiredLength}`;
+        case 'min':
+          return `Min value is ${errors[key].min}`;
+        default:
+      }
+    }
+
+    return null;
+  }
 
   onSave() {
     if (this.myForm.invalid) {
