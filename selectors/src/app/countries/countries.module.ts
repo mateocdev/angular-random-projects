023846ac3cc78ea9810a84893bfc6f7515f3317d
mateocdev <m.castano1@uniandes.edu.co>
2024-05-06 12:53:28 -0500

    🚧 Services and interfaces for country

--- selectors/src/app/countries/countries.module.ts
+++ selectors/src/app/countries/countries.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CountiresRoutingModule } from './countries-routing.module';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 @NgModule({
   declarations: [SelectorPageComponent],
-  imports: [CommonModule, CountiresRoutingModule],
+  imports: [CommonModule, CountiresRoutingModule, ReactiveFormsModule],
 })
 export class CountriesModule {}
