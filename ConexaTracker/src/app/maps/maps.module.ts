f2944141f9df62cf389ffb85c0b5980faaa51d83
Mateo Castano <m.castano1@uniandes.edu.co>
2024-06-30 11:22:57 -0500

    :construction: Structure of the app

--- ConexaTracker/src/app/maps/maps.module.ts
+++ ConexaTracker/src/app/maps/maps.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { MapsRoutingModule } from './maps-routing.module';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
 @NgModule({
-  declarations: [MiniMapComponent],
+  declarations: [MiniMapComponent, SideMenuComponent, MapsLayoutComponent, FullScreenPageComponent, MarkersPageComponent, PropertiesPageComponent, ZoomRangePageComponent],
   imports: [CommonModule, MapsRoutingModule],
 })
 export class MapsModule {}
