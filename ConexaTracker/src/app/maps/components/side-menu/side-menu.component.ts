83e340e92a642e1b4370972a283ea6d7face6018
Mateo Castano <m.castano1@uniandes.edu.co>
2024-07-01 14:25:56 -0500

    :construction: Wip side menu

--- ConexaTracker/src/app/maps/components/side-menu/side-menu.component.ts
+++ ConexaTracker/src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  route: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'FullScreen' },
+    { route: '/maps/zoom-range', name: 'ZoomRange' },
+    { route: '/maps/markers', name: 'Markers' },
+    { route: '/maps/properties', name: 'Properties' },
+  ];
 }
