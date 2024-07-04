65125142d765c9be8d1759a46e70459494898116
Mateo Castano <m.castano1@uniandes.edu.co>
2024-07-04 18:05:04 -0500

    Adding viewChild

--- ConexaTracker/src/app/maps/maps.module.ts
+++ ConexaTracker/src/app/maps/maps.module.ts
@@ -1,17 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import mapboxgl from 'mapbox-gl';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { MapsRoutingModule } from './maps-routing.module';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+(mapboxgl as any).accessToken =
+  'pk.eyJ1IjoibWF0ZW9jZGV2IiwiYSI6ImNseTNlMHVxZzAydnEya3B5enJiaWMwN2gifQ.n88Ro4p-a4Xv4wXoSudGfA';
 
 @NgModule({
-  declarations: [MiniMapComponent, SideMenuComponent, MapsLayoutComponent, FullScreenPageComponent, MarkersPageComponent, PropertiesPageComponent, ZoomRangePageComponent],
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent,
+  ],
   imports: [CommonModule, MapsRoutingModule],
 })
 export class MapsModule {}
--- ConexaTracker/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ ConexaTracker/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrl: './full-screen-page.component.css',
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  constructor() {}
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('divMap is not defined');
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
