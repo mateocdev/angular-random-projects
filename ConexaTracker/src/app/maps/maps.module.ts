f6f21629cee4e2f60580f62bea8511e08e55f885
Mateo Castano <m.castano1@uniandes.edu.co>
2024-07-08 10:00:55 -0500

    Adding new functionalities of zoom

--- ConexaTracker/src/app/maps/maps.module.ts
+++ ConexaTracker/src/app/maps/maps.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
+import { environment } from '../../environments/environment';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
@@ -9,8 +10,7 @@ import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-pa
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
-(mapboxgl as any).accessToken =
-  'pk.eyJ1IjoibWF0ZW9jZGV2IiwiYSI6ImNseTNlMHVxZzAydnEya3B5enJiaWMwN2gifQ.n88Ro4p-a4Xv4wXoSudGfA';
+(mapboxgl as any).accessToken = environment.mapbox_key;
 
 @NgModule({
   declarations: [
--- ConexaTracker/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ ConexaTracker/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.css'
+  styleUrl: './zoom-range-page.component.css',
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+  constructor() {}
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('divMap is not defined');
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+
+    this.mapListener();
+  }
+
+  mapListener() {
+    if (!this.map) throw new Error('map is not defined');
+    this.map.on('zoom', (ev) => (this.zoom = this.map!.getZoom()));
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    if (!this.map) throw new Error('map is not defined');
+    this.map.zoomIn();
+  }
+
+  zoomOut() {
+    if (!this.map) throw new Error('map is not defined');
+    this.map.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    if (!this.map) throw new Error('map is not defined');
+    this.map!.zoomTo(Number(value));
+  }
 }
