45d66813d807c8453d5821b9dc717e803d5e5cbc
mateocdev <m.castano1@uniandes.edu.co>
2024-07-22 09:40:16 -0500

    🚧 Skeleton of files

---
+++ directiveSignals/src/app/products/products.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ProductsRoutingModule } from './products-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ProductsRoutingModule],
+})
+export class ProductsModule {}
