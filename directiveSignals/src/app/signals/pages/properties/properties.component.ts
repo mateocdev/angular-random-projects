dda0633b46ae3d5bc90eb3d1c20b751df09474e6
mateocdev <m.castano1@uniandes.edu.co>
2024-08-06 11:23:23 -0500

    🚧 Adding signal effect

--- directiveSignals/src/app/signals/pages/properties/properties.component.ts
+++ directiveSignals/src/app/signals/pages/properties/properties.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties.component.html',
-  styleUrl: './properties.component.css'
+  styleUrl: './properties.component.css',
 })
 export class PropertiesComponent {
+  public user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg',
+  });
 
+  public userChangedEffect = effect(() => {
+    console.log('User changed', this.user()?.first_name);
+  });
+
+  onFieldUpdated(field: string, value: string) {
+    this.user.update((user) => {
+      switch (field) {
+        case 'email':
+          user.email = value;
+          break;
+        case 'first_name':
+          user.first_name = value;
+          break;
+        case 'last_name':
+          user.last_name = value;
+          break;
+        case 'avatar':
+          user.avatar = value;
+          break;
+      }
+      return user;
+    });
+  }
 }
