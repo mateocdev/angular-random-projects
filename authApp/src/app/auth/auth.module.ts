356b11e472ad6c3f2142a535ec5c345184b48640
Mateo <m.castano1@uniandes.edu.co>
2024-08-22 16:13:49 -0500

    Adding skeleton from platform

--- authApp/src/app/auth/auth.module.ts
+++ authApp/src/app/auth/auth.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [
+    LoginPageComponent,
+    RegisterPageComponent,
+    AuthLayoutComponent,
+  ],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- authApp/src/app/dashboard/dashboard.module.ts
+++ authApp/src/app/dashboard/dashboard.module.ts
@@ -1,19 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { LayoutsdashboardLayoutComponent } from './layoutsdashboard-layout/layoutsdashboard-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
-
 @NgModule({
-  declarations: [
-    LayoutsdashboardLayoutComponent,
-    DashboardLayoutComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardLayoutComponent],
+  imports: [CommonModule, DashboardRoutingModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
