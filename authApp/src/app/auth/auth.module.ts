696f3fec995fc6b82b1cec279b33744770fc4340
Mateo <m.castano1@uniandes.edu.co>
2024-08-22 16:35:56 -0500

    Adding new styles

--- authApp/src/app/auth/auth.module.ts
+++ authApp/src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -12,6 +13,6 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     RegisterPageComponent,
     AuthLayoutComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [ReactiveFormsModule, CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
